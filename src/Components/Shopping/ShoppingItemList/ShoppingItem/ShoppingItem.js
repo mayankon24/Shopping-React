6168775f7f46298fe09d32cf60a6b7dcfb5b6f18
mayank <mayankaggarwal.cse@gmail.com>
2020-03-15 20:09:16 +0530

    add cart functionality

--- src/Components/Shopping/ShoppingItemList/ShoppingItem/ShoppingItem.js
+++ src/Components/Shopping/ShoppingItemList/ShoppingItem/ShoppingItem.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {useDispatch} from 'react-redux'
+import * as Action from '../../../../Store/Actions/Index'
 import './ShoppingItem.css'
 
 
 
 const ShoppingItem = ({id, name, price, discount, category, img_url, sellingPrice, discountPercentage}) =>
 {
-
+    const [itemId] = useState(id);
+    const dispatch = useDispatch();
    
+    const handelAddCartClick = ()=>{
+        dispatch(Action.addCartItem(itemId) );
+    }
+
+
     return(
         <div className="div-container">
             <img src = {img_url} height = "150" width = "100%"  alt="" />
@@ -18,7 +26,7 @@ const ShoppingItem = ({id, name, price, discount, category, img_url, sellingPric
                 <span className="text-discount"> {discountPercentage}%  off  </span> 
             </div>
             <div className="div-button"> 
-                <button className= "button roundButton" >Add to Cart </button>
+                <button className= "button roundButton" onClick = {handelAddCartClick} >Add to Cart </button>
             </div>
         </div>
     )
